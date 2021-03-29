a1ddf8c1778bbe208b5bfa866b92a714004b9033
xinqipei <xinqipei@gmail.com>
2021-03-29 22:41:35 +0200

    Fix JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-import Rails from "@rails/ujs"
+/*import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
@@ -6,3 +6,4 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+*/
